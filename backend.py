78cc46384569cac815b4b67b6183442408aaa562
HimanshuBungla <https://github.com/HimanshuBungla>
2025-08-13 13:26:22 +0530

    Added Requirements file

--- backend.py
+++ backend.py
@@ -4,7 +4,10 @@ from typing import TypedDict, Literal, Annotated
 try:
     from langchain_openai import ChatOpenAI
 except ImportError:
-    from langchain.chat_models import ChatOpenAI
+    try:
+        from langchain.chat_models import ChatOpenAI
+    except ImportError:
+        from langchain_community.chat_models import ChatOpenAI
 
 from langchain.schema import SystemMessage, HumanMessage, BaseMessage
 from dotenv import load_dotenv
